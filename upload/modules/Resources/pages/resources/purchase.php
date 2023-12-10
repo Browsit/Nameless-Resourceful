92c57d22e92f5632e9bc07c0c36d474a387aee34
PikaMug <2267126+PikaMug@users.noreply.github.com>
2023-12-09 20:21:45 -0500

    Add experience profile. Bump version

--- upload/modules/Resources/pages/resources/purchase.php
+++ upload/modules/Resources/pages/resources/purchase.php
@@ -162,6 +162,8 @@ if(isset($_GET['do'])){
                         $amount->setTotal($resource->price, $resource->discount);
                         $amount->setCurrency($currency);
 
+                        $exp_id = json_decode(getExpProfileId($apiContext), true);
+
                         $transaction = new \PayPal\Api\Transaction();
                         $transaction->setAmount($amount);
                         $transaction->setPayee($payee);
@@ -174,6 +176,7 @@ if(isset($_GET['do'])){
                         $payment = new \PayPal\Api\Payment();
                         $payment->setIntent('sale')
                             ->setPayer($payer)
+                            ->setExperienceProfileId($exp_id['id'])
                             ->setTransactions([$transaction])
                             ->setRedirectUrls($redirectUrls);
 
@@ -183,8 +186,10 @@ if(isset($_GET['do'])){
                             Redirect::to($payment->getApprovalLink());
 
                         } catch (\PayPal\Exception\PayPalConnectionException $ex) {
-                            ErrorHandler::logCustomError($ex->getData());
-                            $error = $resource_language->get('resources', 'error_while_purchasing');
+                            $ex_data = $ex->getData();
+                            ErrorHandler::logCustomError($ex_data);
+                            $ex_msg = json_decode($ex_data, true)['message'];
+                            $error = $resource_language->get('resources', 'error_while_purchasing') . " (" . $ex_msg . ")";
 
                         }
                     }
@@ -261,3 +266,62 @@ require(ROOT_PATH . '/core/templates/navbar.php');
 require(ROOT_PATH . '/core/templates/footer.php');
 
 $template->displayTemplate($template_file, $smarty);
+
+function getExpProfileId($webContext) {
+
+    $flowConfig = new \PayPal\Api\FlowConfig();
+    // Type of PayPal page to be displayed when a user lands on the PayPal site for checkout. Allowed values: Billing or Login. When set to Billing, the Non-PayPal account landing page is used. When set to Login, the PayPal account login landing page is used.
+    $flowConfig->setLandingPageType("Billing");
+    // The URL on the merchant site for transferring to after a bank transfer payment.
+    $flowConfig->setBankTxnPendingUrl(rtrim(URL::getSelfURL(), '/'));
+    // When set to "commit", the buyer is shown an amount, and the button text will read "Pay Now" on the checkout page.
+    $flowConfig->setUserAction("commit");
+    // Defines the HTTP method to use to redirect the user to a return URL. A valid value is `GET` or `POST`.
+    $flowConfig->setReturnUriHttpMethod("GET");
+
+    // Parameters for style and presentation.
+    //$presentation = new \PayPal\Api\Presentation();
+
+    // A URL to logo image. Allowed values: .gif, .jpg, or .png.
+    //$presentation->setLogoImage("https://www.browsit.org/favicon.ico")
+        //	A label that overrides the business name in the PayPal account on the PayPal pages.
+        //->setBrandName("SiteName")
+        //  Locale of pages displayed by PayPal payment experience.
+        //->setLocaleCode("US")
+        // A label to use as hypertext for the return to merchant link.
+        //->setReturnUrlLabel("Return")
+        // A label to use as the title for the note to seller field. Used only when `allow_note` is `1`.
+        //->setNoteToSellerLabel("Thanks!");
+
+    // Parameters for input fields customization.
+    $inputFields = new \PayPal\Api\InputFields();
+    // Enables the buyer to enter a note to the merchant on the PayPal page during checkout.
+    $inputFields->setAllowNote(false)
+        // Determines whether or not PayPal displays shipping address fields on the experience pages. Allowed values: 0, 1, or 2. When set to 0, PayPal displays the shipping address on the PayPal pages. When set to 1, PayPal does not display shipping address fields whatsoever. When set to 2, if you do not pass the shipping address, PayPal obtains it from the buyer’s account profile. For digital goods, this field is required, and you must set it to 1.
+        ->setNoShipping(1)
+        // Determines whether or not the PayPal pages should display the shipping address and not the shipping address on file with PayPal for this buyer. Displaying the PayPal street address on file does not allow the buyer to edit that address. Allowed values: 0 or 1. When set to 0, the PayPal pages should not display the shipping address. When set to 1, the PayPal pages should display the shipping address.
+        ->setAddressOverride(0);
+
+    // #### Payment Web experience profile resource
+    $webProfile = new \PayPal\Api\WebProfile();
+
+    // Name of the web experience profile. Required. Must be unique
+    $webProfile->setName("NamelessMC" . uniqid())
+        // Parameters for flow configuration.
+        ->setFlowConfig($flowConfig)
+        // Parameters for style and presentation.
+        //->setPresentation($presentation)
+        // Parameters for input field customization.
+        ->setInputFields($inputFields)
+        // Indicates whether the profile persists for three hours or permanently. Set to `false` to persist the profile permanently. Set to `true` to persist the profile for three hours.
+        ->setTemporary(true);
+
+    try {
+        $createProfileResponse = $webProfile->create($webContext);
+    } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+        ErrorHandler::logCustomError($ex->getData());
+        exit(1);
+    }
+
+    return $createProfileResponse;
+}
